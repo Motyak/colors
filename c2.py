9db2bc34ab9c48cfa9d0334b3a9ba30f0b0db4b6
Tommy Anfosso <anfossotommy@hotmail.fr>
2021-08-11 05:48:01 +0200

    its bad

--- c2.py
+++ c2.py
@@ -17,37 +17,42 @@ def _hexColorToHsl(str):
     # hls to proper hsl
     return (hls[0] * 360, hls[2] * 100, hls[1] * 100)
 
-def _compareHsl(a, b):
-    hsl1 = HSLColor(a[0] / 360, hsl_s=a[1] / 100, hsl_l=a[2] / 100)
-    hsl2 = HSLColor(b[0] / 360, hsl_s=b[1] / 100, hsl_l=b[2] / 100)
-    lab1 = convert_color(hsl1, LabColor)
-    lab2 = convert_color(hsl2, LabColor)
-    return delta_e_cie1976(lab1, lab2)
+def compareHexColors(a, b):
+    RANGES = range(0, 361, 30)
+    hsl1, hsl2 = _hexColorToHsl(a), _hexColorToHsl(b)
 
-COLOR_RANGE_HSL = [nmToHsl(i) for i in range (380, 781)]
+    for i in RANGES:
+        if hsl1[0]>=i and hsl1[0]<i+30 and hsl2[0]>=i and hsl2[0]<i+30 and hsl1[0]>hsl2[0]:
+            return 1
+    if hsl1[2]!=b[2] and hsl1[2]<hsl2[2]:
+        return -1 
+    else:
+        return 0
 
-# fonction qui retourne la longueur d'onde la plus proche du code hex
-def closestWavelength(hexColor):
-    hslInput = _hexColorToHsl(hexColor)
-    closestHsl = (-1, -1, -1)
-    closestWavelength = -1
-    smallestDifference = 999999
+# COLOR_RANGE_HSL = [nmToHsl(i) for i in range (380, 781)]
+
+# # fonction qui retourne la longueur d'onde la plus proche du code hex
+# def closestWavelength(hexColor):
+#     hslInput = _hexColorToHsl(hexColor)
+#     closestHsl = (-1, -1, -1)
+#     closestWavelength = -1
+#     smallestDifference = 999999
     
-    for index, c in enumerate(COLOR_RANGE_HSL):
-        diff = _compareHsl(hslInput, c)
-        if diff < smallestDifference:
-            # print("smallest was", smallestDifference, "now it's", diff)#debug
-            closestHsl = c
-            closestWavelength = index + 380
-            smallestDifference = diff
-        # print(c, diff)#debug
+#     for index, c in enumerate(COLOR_RANGE_HSL):
+#         diff = _compareHsl(hslInput, c)
+#         if diff < smallestDifference:
+#             # print("smallest was", smallestDifference, "now it's", diff)#debug
+#             closestHsl = c
+#             closestWavelength = index + 380
+#             smallestDifference = diff
+#         # print(c, diff)#debug
 
-    return closestWavelength
+#     return closestWavelength
 
 if __name__ == '__main__':
     print(_hexColorToHsl("#142857"))
-    print(_compareHsl(nmToHsl(500), nmToHsl(382)))
-    print(closestWavelength("#593406"))
+    print(_compareHexColors("#142857", "#010989"))
+    # print(closestWavelength("#593406"))
 
 
 
--- c3.py
+++ c3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-from c2 import closestWavelength
+from c2 import compareHexColors
+
+from functools import cmp_to_key
 
 def nbToHexColor(nb):
     return '#%06d' % nb
@@ -9,8 +11,7 @@ def sumOfDigits(n):
 
 seq = [i*999999//91 for i in range(1,91) if i%10!=0 and sumOfDigits(i)<10]
 hexColors = [*map(nbToHexColor, seq)]
-sortedHexColors = [*sorted(hexColors, key=closestWavelength)]
+sortedHexColors = [*sorted(hexColors, key=cmp_to_key(compareHexColors))]
 
 for c in sortedHexColors:
     print(c)
-
